c8cb15a713d4ea388a1f9229e6d89d5eb7d7d34f
Rohde Fischer <rohdef@rohdef.dk>
2012-02-19 15:03:00 +0100

    Encryption working

--- src/main/java/dk/rohdef/DMS/DMSMainActivity.java
+++ src/main/java/dk/rohdef/DMS/DMSMainActivity.java
@@ -1,7 +1,23 @@
 package dk.rohdef.DMS;
 
-import java.util.GregorianCalendar;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.UUID;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 import org.xmlrpc.android.XMLRPCClient;
 import org.xmlrpc.android.XMLRPCException;
@@ -91,8 +107,8 @@ public class DMSMainActivity extends Activity {
 			// TODO argh, int please! (Or something similar)
 	    	String phone = preferences.getString("phone", "21680621");
 			String uuid = UUID.randomUUID().toString();
-			String signature = phone+"\n"+uuid;
-			signature = "rulle";
+			
+			byte[] signature = createSignature(phone, uuid);
 			
 			XMLRPCClient xmlRpcClient =
 					new XMLRPCClient("http://192.168.2.166:8080/AlarmService/xmlrpc");
@@ -107,6 +123,66 @@ public class DMSMainActivity extends Activity {
 				Log.e("XML RPC call", "Failed", e);
 			}
 		}
+
+		private byte[] createSignature(String phone, String uuid) {
+			String rawSignature = phone + uuid;
+
+			PrivateKey key = readPrivateKeyFromFile();
+			byte[] cipherData = null;
+			try {
+				Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+				cipher.init(Cipher.ENCRYPT_MODE, key);
+				cipherData = cipher.doFinal(rawSignature.getBytes("UTF8"));
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			} catch (NoSuchPaddingException e) {
+				e.printStackTrace();
+			} catch (InvalidKeyException e) {
+				e.printStackTrace();
+			} catch (IllegalBlockSizeException e) {
+				e.printStackTrace();
+			} catch (BadPaddingException e) {
+				e.printStackTrace();
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+			
+			return cipherData;
+		}
+
+		private PrivateKey readPrivateKeyFromFile() {
+			PrivateKey key = null;
+			try {
+				InputStream fileStream = getResources()
+						.openRawResource(R.raw.mykey);
+				DataInputStream dataInput = new DataInputStream(
+						fileStream);
+
+				byte[] buffer = new byte[1024];
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				while (true) {
+					int n = dataInput.read(buffer);
+					if (n<0) break;
+					baos.write(buffer, 0, n);
+				}
+				
+				dataInput.close();
+				
+				PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(baos.toByteArray());
+				KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+				key = keyFactory.generatePrivate(spec);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			} catch (InvalidKeySpecException e) {
+				Log.e("Open private key", "Invalid key specification", e);
+			}
+			return key;
+		}
+
 
 		@Override
 		public void onTick(long millisUntilFinished) {
